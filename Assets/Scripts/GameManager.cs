e3cc0c32d357b54444a9e73b9c152b7a85eda4aa
Sebastiao Carvalho <sebastiaovscarvalho@tecnico.ulisboa.pt>
2024-01-27 12:27:01 +0000

    add lives and player respawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     GameObject[] _players = new GameObject[2];
+    
     // Start is called before the first frame update
     void Start()
     {
-        _players[0] = GameObject.Find("Player1");
-        _players[1] = GameObject.Find("Player2");
+        _players[0] = GameObject.Find("Player1").transform.GetChild(0).gameObject;
+        _players[1] = GameObject.Find("Player2").transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (GameObject player in _players) {
+            Player playerScript = player.GetComponent<Player>();
+            if (playerScript.OutOfBounds()) {
+                if (playerScript.Lives > 1)
+                    playerScript.Respawn();
+                else
+                    Debug.Log("Game Over");
+            }
+        }    
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private float _hp = 10f;
+    private float _hp = 0f;
+    private int _lives = 3;
+
+    public int Lives {
+        get { return _lives; }
+    }
+    private Vector3 _startPosition;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _startPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -19,7 +25,25 @@ public class Player : MonoBehaviour
     }
 
     public void TakeDamage(float damage) {
-        _hp -= damage;
+        _hp += damage;
         Debug.Log("Player HP: " + _hp);
+    }
+
+    public bool OutOfBounds() {
+        return transform.position.y < -10;
+    }
+
+    public void Respawn() {
+        _lives--;
+        transform.position = _startPosition;
+        _hp = 0f;
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("Player Collided");
+        if (other.gameObject.CompareTag("Attack")) {
+            TakeDamage(other.gameObject.GetComponent<Attack>().Damage);
+            Destroy(other.gameObject);
+        }
     }
 }
